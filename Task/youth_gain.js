5863635ef754b5c049dcddf1e646915b96d373bd
QingAnLe <69788366+QingAnLe@users.noreply.github.com>
2021-03-24 21:00:27 +0800

    Update youth_gain.js

--- Task/youth_gain.js
+++ Task/youth_gain.js
@@ -105,7 +105,7 @@ function GainStart() {
                 $.setdata(smbody, 'youth_start');
                 $.log(startres.message + "已自动删除")
             } else {
-                comstate = startres.items.comtele_state;
+                comstate = startres.items.comtele_state==0;
                 if (comstate == 0) {
                     $.log("任务开始，" + startres.items.banner_id + startres.message);
                     await $.wait(10000);
@@ -127,7 +127,7 @@ function lookStart() {
                 $.setdata(smbody, 'youth_look');
                 $.log(startlk.message + "已自动删除")
             } else {
-                comstate = startlk.items.comtele_state==0;
+                comstate = startlk.items.comtele_state==1;
                 if (comstate == 0) {
                     $.log("任务开始，" + startlk.items.banner_id + startlk.message);
                     for (let j = 0; j < startlk.items.see_num - startlk.items.read_num; j++) {
